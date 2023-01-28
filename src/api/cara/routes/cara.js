c1d4b88e69f87d6a306389ba2ed415873f5eb35a
Fredy Polania <fpolania@gmail.com>
2023-01-28 13:45:54 -0400

    add: Collections

---
+++ src/api/cara/routes/cara.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cara router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cara.cara');
---
+++ src/api/cara/services/cara.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cara service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::cara.cara');
